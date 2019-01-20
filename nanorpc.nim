af7a13ea1981d7a547b510c90541017b776dde77
zargot <zargot.ripley@gmail.com>
2019-01-20 14:20:40 +0100

    add request error msg

--- nanorpc.nim
+++ nanorpc.nim
@@ -27,6 +27,9 @@ converter toString(n: JsonNode): string =
 template getProcName(): string =
     $getFrame().procname
 
+template error(msg) =
+    echo "RPC error: ", msg
+
 proc newNanoRPC(): NanoRPC =
     result.new()
     result.client = newHttpClient()
@@ -36,12 +39,15 @@ proc request(self: NanoRPC, body: JsonNode): (bool, JsonNode) =
     try:
         let res = httpclient.request(client, url, HttpPost, $body)
         if res.code != 200.HttpCode:
+            error res.repr
             return
         let data = res.body.parseJson
         if data.hasKey("error"):
+            error data["error"].getStr
             return
         return (true, data)
     except:
+        error getCurrentExceptionMsg()
         discard
 
 proc account_balance*(self: NanoRPC, acc: string): (bool, Balance) =
